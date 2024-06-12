e5d2a56221f5a976828f0dc51b6e8baf0136bb91
Stefan Benincasa <stefan.benincasa@protonmail.com>
2024-06-12 15:29:20 +1000

    meditation toggle & context addition;

--- client/src/components/Beacon.js
+++ client/src/components/Beacon.js
@@ -1,29 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
+import React, { useEffect, useState, useContext, useRef } from 'react'
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom'
 
+import { Context } from './Context'
 import { Icon } from './Icon.js'
 
-import '../styles/App.css';
+import '../styles/App.css'
 
 function Beacon() {
-  const [ classes, setClasses ] = useState("meditate");
+  const [ buttonClasses, setButtonClasses ] = useState('')
+  const [ meditationClasses, setMeditationClasses ] = useState([])
+
+  const { isMeditating, setIsMeditating } = useContext(Context)
 
   useEffect(() => {
-  }, [setClasses]) 
+    let newMeditationClasses
+    if(isMeditating) {
+      newMeditationClasses = meditationClasses.filter(item => item != 'meditate')
+    }
+    else {
+      meditationClasses.push('meditate')
+      newMeditationClasses = [ ...meditationClasses ]
+    }
 
-  const toggleMeditation = function(e, classes) {
-    e.preventDefault()
+    setMeditationClasses(newMeditationClasses)
+    
+  }, [isMeditating])
+
+  const toggleMeditation = function(e) {
+    setIsMeditating(currentState => !currentState)
+  }
+
+  const removeClass = function(currentClasses, classForRemoval) {
+    if(!currentClasses) return []
+    return currentClasses.filter(classs => classs != classForRemoval)
+  }
+
+  const addClass = function(currentClasses, classForAddition) {
+    if(!currentClasses) return []
+    return currentClasses.push(classForAddition)
   }
 
   return (
-    <div id="Beacon" 
-    className="p-5 m-auto container-fluid d-flex flex-column align-items-center justify-content-center">
-      <button className={classes} onClick={e => toggleMeditation(e, classes)}>
+    <div id='Beacon' className='p-5 m-auto container-fluid d-flex flex-column align-items-center justify-content-center'>
+      <button className={meditationClasses.join(' ')} onClick={toggleMeditation}>
         Sample Text
       </button>
     </div>
   )
 }
 
-export default Beacon;
+export default Beacon
 
