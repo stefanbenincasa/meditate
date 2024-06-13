bfdedc190a151836eda420b6ea934da8dd3580e1
Stefan Benincasa <stefan.benincasa@protonmail.com>
2024-06-13 10:17:58 +1000

    initial designs for control layout; color focus;

---
+++ client/src/components/ControlPanel.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState, useContext, useRef } from 'react'
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom'
+
+import { Context } from './Context'
+import { Icon } from './Icon.js'
+
+import '../styles/App.css'
+
+// To Do:
+// Color change options toggle
+// Sound effect change toggle 
+function ControlPanel() {
+    const [ colors, setColors ] = useState([])
+    const [ soundEffects, setSoundEffects ] = useState([])
+    
+    return (
+        <nav className='row w-75 mx-auto'>
+            <div className='col-6'>
+                <div className='row'>
+                    <label className='col-3 bg-secondary text-white d-flex flex-column justify-content-center align-items-center'>Colors</label>
+                    <span className='col ms-5'>Color Option</span>
+                    <span className='col ms-5'>Color Option</span>
+                    <span className='col ms-5'>Color Option</span>
+                </div>
+            </div>
+
+            {/* <div className='col-6'>Sound Effects</div> */}
+        </nav>     
+    )
+}
+
+export default ControlPanel
