9bb75ccb0d0532749c35ddcdc190885ce4e907a6
Stefan Benincasa <stefan.benincasa@protonmail.com>
2024-06-13 10:21:11 +1000

    style touchups;

--- client/src/components/Beacon.js
+++ client/src/components/Beacon.js
@@ -28,8 +28,8 @@ function Beacon() {
   }
 
   return (
-    <div id='Beacon' className='p-5 m-auto container-fluid d-flex flex-column align-items-center justify-content-center'>
-      <button className={meditationClasses.join(' ')} onClick={toggleMeditation}>
+    <div id='Beacon' className='h-75 w-75 p-5 mx-auto container-fluid d-flex flex-column align-items-center justify-content-center'>
+      <button className={meditationClasses.join(' ') + 'h-50 w-50'} onClick={toggleMeditation}>
         Sample Text
       </button>
     </div>
